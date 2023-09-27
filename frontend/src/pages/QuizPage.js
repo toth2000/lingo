f5dc53e483835cf7d9620aed0f0acfbe18942b62
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-27 21:21:36 +0530

    [UI]: add quiz page

---
+++ frontend/src/pages/QuizPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QuestionBox from "../components/QuestionBox";
+import { Container } from "./style";
+
+const QuizPage = () => {
+  return (
+    <Container>
+      <QuestionBox />
+    </Container>
+  );
+};
+
+export default QuizPage;
---
+++ frontend/src/pages/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  flex-grow: 1;
+  margin: 2% 0 0 0;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const LeftWrapper = styled.div``;
+
+export const RightWrapper = styled.div``;
