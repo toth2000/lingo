3d0473642814fae8fbadc70642553828e957396c
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-29 00:25:55 +0530

    [UI]: make Choice component dynamic

--- frontend/src/components/Choice/index.js
+++ frontend/src/components/Choice/index.js
@@ -1,11 +1,11 @@
 import { ChoiceText, Container, TextContainer } from "./style";
 
-const Choice = () => {
+const Choice = ({ index, showOrder, children, active, onClick }) => {
   return (
-    <Container active={false}>
+    <Container active={active} onClick={() => onClick(index)}>
       <TextContainer>
-        <ChoiceText> 1.</ChoiceText>
-        <ChoiceText>an</ChoiceText>
+        {showOrder ? <ChoiceText> {`${index + 1}.`}</ChoiceText> : null}
+        {children}
       </TextContainer>
     </Container>
   );
