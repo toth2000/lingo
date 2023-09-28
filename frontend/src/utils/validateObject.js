8060b3375dc9c49351a438bf02f16e4c9124b645
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-28 20:36:23 +0530

    [UI]: update validation util to handle edge cases

--- frontend/src/utils/validateObject.js
+++ frontend/src/utils/validateObject.js
@@ -1,4 +1,9 @@
 export const validateObject = (object, keys = Object.keys(object)) => {
-  const valid = keys.every((item) => (object[item] ? true : false));
-  return valid;
+  try {
+    const valid = keys.every((item) => (object[item] ? true : false));
+    return valid;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
