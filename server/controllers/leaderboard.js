8e527dc893f14f00e8ddfb8a58fb0fee5fb6b5eb
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-29 02:22:14 +0530

    [Server]: Leaderboard include name

--- server/controllers/leaderboard.js
+++ server/controllers/leaderboard.js
@@ -1,12 +1,28 @@
 const Statistic = require("../models/Statistic");
+const User = require("../models/User");
 
 const getLeaderboard = async (req, res) => {
   try {
-    const result = await Statistic.find().sort({ points: -1 }).limit(10);
-    res.json(result);
+    // Upgrade to Aggregate pending
+    const progressData = await Statistic.find().sort({ points: -1 }).limit(10);
+
+    const userIds = progressData.map((item) => item._id);
+
+    const userData = await User.find({ _id: { $in: userIds } });
+
+    const data = {};
+
+    userData.forEach((item) => (data[item._id] = item.name));
+
+    const result = progressData.map((item) => ({
+      ...item._doc,
+      name: data[item._id],
+    }));
+
+    res.status(200).json(result);
   } catch (error) {
     console.log("Error: ", error);
-    res.json({
+    res.status(500).json({
       message: "An error occured, please try again later",
       error: error,
     });
