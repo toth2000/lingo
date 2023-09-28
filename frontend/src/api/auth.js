7b65a1491f8a5ffccde3d042c34b04a64d9ba908
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-28 20:41:55 +0530

    [UI]: add authentication api call

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,24 @@
 import { API } from ".";
-import { REFRESH_TOKEN_ENDPOINT } from "../constant/apiUrl";
+import {
+  LOGIN_ENDPOINT,
+  REFRESH_TOKEN_ENDPOINT,
+  REGISTER_USER_ENDPOINT,
+} from "../constant/apiUrl";
+
+export const login = ({ email, password }) => {
+  return API.post(`${LOGIN_ENDPOINT}`, {
+    email: email,
+    password: password,
+  });
+};
+
+export const register = ({ name, email, password }) => {
+  return API.post(`${REGISTER_USER_ENDPOINT}`, {
+    name: name,
+    email: email,
+    password: password,
+  });
+};
 
 export const refreshToken = (refreshToken) => {
   return API.post(`${REFRESH_TOKEN_ENDPOINT}`, {
--- frontend/src/constant/apiUrl.js
+++ frontend/src/constant/apiUrl.js
@@ -1,5 +1,7 @@
-export const API_URL = "http://localhost:4000";
-
+export const API_URL = "https://edf4-117-205-183-248.ngrok-free.app";
+// "http://localhost:4000";
 
 // AUTH ROUTE
-export const REFRESH_TOKEN_ENDPOINT = "/auth/refresh"
+export const LOGIN_ENDPOINT = "/auth/login";
+export const REGISTER_USER_ENDPOINT = "/auth/register";
+export const REFRESH_TOKEN_ENDPOINT = "/auth/refresh";
