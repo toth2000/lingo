bed085b11b0d0ac4029eeb3eb37f59b96bf5a7c9
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-29 10:46:53 +0530

    [Server]: add middleware to fetch access token

--- server/routes/language.js
+++ server/routes/language.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { addLanguage, addLevel, getLanguage } = require("../controllers/language");
+const { verifyTokenMiddleware } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", getLanguage);
+router.get("/", verifyTokenMiddleware ,getLanguage);
 
 // Create new language
 router.post("/", addLanguage);
