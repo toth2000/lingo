dd6d272791062e4bdbb4baa56ea6444e3fcf5c64
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-09-29 11:19:42 +0530

    [UI]: make navbar link active

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -5,19 +5,53 @@ import logo from "../../icons/logo-48.png";
 import userIcon from "../../icons/user.png";
 import trophyIcon from "../../icons/trophy.png";
 import pathIcon from "../../icons/path.png";
+import { Link } from "react-router-dom";
+import { GITHUB_REPO_URL } from "../../constant/url";
+import {
+  HOME_ROUTE,
+  LEADERBOARD_ROUTE,
+  LEARNING_PATH_ROUTE,
+  USER_ROUTE,
+} from "../../constant/routes";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { authState, isUserAuthenticated } = useContext(AuthContext);
+
+  const [userRoute, setUserRoute] = useState(HOME_ROUTE);
+
+  useEffect(() => {
+    if (isUserAuthenticated()) {
+      const url = USER_ROUTE.split(":")[0];
+      const userId = authState.userId;
+      setUserRoute(`${url}${userId}`);
+    } else {
+      setUserRoute(HOME_ROUTE);
+    }
+  }, [authState]);
+
   return (
     <Container>
-      <Wrapper gap="8px">
-        <Logo src={logo} />
-        <Title>Lingo</Title>
-      </Wrapper>
+      <Link to={HOME_ROUTE} style={{ textDecoration: "none" }}>
+        <Wrapper gap="8px">
+          <Logo src={logo} />
+          <Title>Lingo</Title>
+        </Wrapper>
+      </Link>
       <Wrapper gap="25px">
-        <Icon src={githubIcon} />
-        <Icon src={pathIcon} />
-        <Icon src={trophyIcon} />
-        <Icon src={userIcon} />
+        <Link to={GITHUB_REPO_URL}>
+          <Icon src={githubIcon} />
+        </Link>
+        <Link to={LEARNING_PATH_ROUTE}>
+          <Icon src={pathIcon} />
+        </Link>
+        <Link to={LEADERBOARD_ROUTE}>
+          <Icon src={trophyIcon} />
+        </Link>
+        <Link to={userRoute}>
+          <Icon src={userIcon} />
+        </Link>
       </Wrapper>
     </Container>
   );
---
+++ frontend/src/constant/url.js
@@ -0,0 +1,2 @@
+export const WEBSITE_URL = "https://lingo-pi.vercel.app";
+export const GITHUB_REPO_URL = "https://github.com/toth2000/lingo";
